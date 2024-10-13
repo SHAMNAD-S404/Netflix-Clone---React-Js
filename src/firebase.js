ea0e223268e16886f9a5f55a9067ad352263748e
SHAMNAD-S404 <shamnadsakeer306@gmail.com>
2024-10-13 23:37:59 +0530

    changed firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,71 +1,63 @@
- 
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_APIKEY ,
+  apiKey: import.meta.env.VITE_REACT_APP_APIKEY,
   authDomain: import.meta.env.VITE_REACT_APP_AUTHDOM,
   projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
   storageBucket: import.meta.env.VITE_REACT_APP_STORE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_REACT_APP_MSG_ID ,
-  appId: import.meta.env.VITE_REACT_APP_APPID  
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MSG_ID,
+  appId: import.meta.env.VITE_REACT_APP_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const db   = getFirestore(app)
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-const signUp = async (name,email,password) => {
-
-    try {
-
-        const response = await createUserWithEmailAndPassword(auth,email,password);
-        const user = response.user;
-        await addDoc(collection(db,'local'),{
-          uid : user.uid,
-          name,
-          authProvider:'local',
-          email
-        })
-
-    } catch (error) {
-        console.error(error);
-        toast.error(error.code.split('/')[1].split('-').join(' '));
-    }
-}
-
-const login = async (email,password) => {
-
+const signUp = async (name, email, password) => {
   try {
-
-    await signInWithEmailAndPassword(auth,email,password)
-    
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = response.user;
+    await addDoc(collection(db, "local"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
   } catch (error) {
-
     console.error(error);
-    toast.error(error.code.split('/')[1].split('-').join(' '));
-    
+    toast.error(error.code.split("/")[1].split("-").join(" "));
   }
-}
+};
 
-const logout  = async()=> {
-
+const login = async (email, password) => {
   try {
-    signOut(auth)
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = async () => {
+  try {
+    signOut(auth);
   } catch (error) {
     console.log(error);
-    toast.error(error.code.split('/')[1].split('-').join(' '));
+    toast.error(error.code.split("/")[1].split("-").join(" "));
   }
-}
+};
 
-export {
-
-  auth,
-  db,
-  login,
-  logout,
-  signUp
-}
+export { auth, db, login, logout, signUp };
